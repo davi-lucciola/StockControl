656309ba71875dfb4853f22e00c69f264d276206
Davi Lucciola <davilucciola14@gmail.com>
2023-11-14 19:02:34 -0300

    feat: mamacos.json

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .controllers import product_router, stock_router
 
 
 stock_control = FastAPI()
+
+stock_control.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 # Routes
 stock_control.include_router(product_router)

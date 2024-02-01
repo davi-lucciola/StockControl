cd84440304e742f92239513b30489bbf36f87696
Davi Lucciola <davilucciola14@gmail.com>
2024-02-01 12:12:20 -0300

    feat: api finalizada

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .controllers import product_router, stock_router
 
 
-stock_control = FastAPI()
+stock_control = FastAPI(title='API Controle de Estoque')
 
 stock_control.add_middleware(
     CORSMiddleware,
@@ -16,7 +16,3 @@ stock_control.add_middleware(
 # Routes
 stock_control.include_router(product_router)
 stock_control.include_router(stock_router)
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run('main:stock_control', reload=True)
